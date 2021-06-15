6168ebb8cc62e5ab3e85e8b5d874b75b70da7c93
Patrick May <patrickpm.may@gmail.com>
2021-06-15 12:21:28 -0500

    create template layout

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
+posts = [
+    {
+        "author":"Rand Al-Thor",
+        "title":"Blog Post 1",
+        "content":"First Post content",
+        "date_posted":"May 1st"
+    },
+    {
+        "author":"Anasurimbor Kellhus",
+        "title":"Blog Post 2",
+        "content":"Second Post content",
+        "date_posted":"May 5th"
+    },
+    {
+        "author":"Paul Atreides",
+        "title":"Blog Post 3",
+        "content":"Third Post content",
+        "date_posted":"May 7th"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "About Me"
+    return render_template("about.html", title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
