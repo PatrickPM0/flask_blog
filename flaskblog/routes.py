0ba3d8ff9362b9433ccffa96718b97ce60ce2f11
Patrick May <patrickpm.may@gmail.com>
2021-08-06 12:32:46 -0500

    update login_manager and add next_page functionali

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,6 @@
-import flask_login
-from flask_login.utils import login_user, current_user, logout_user
+from flask_login.utils import login_user, current_user, logout_user, login_required
 from flaskblog.models import User, Post
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog import app, db, bcrypt
 
@@ -58,7 +57,11 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get("next")
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -68,3 +71,9 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("home"))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
