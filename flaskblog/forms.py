bc5f5ee0ea30f95d708cea16f745ac8f188f97c4
Patrick May <patrickpm.may@gmail.com>
2021-08-11 12:35:14 -0500

    delete post functionality

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -63,4 +63,4 @@ class UpdateAccountForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     content = TextAreaField("Content", validators=[DataRequired()])
-    submit = SubmitField("Post")
+    submit = SubmitField("Submit")
